8ca81af5e046f2f695cf8447b785f0b42da9edc1
vishudhGupta <vishudhgupta001@gmail.com>
2023-11-04 09:26:16 +0530

    Intial Commit

---
+++ src/myComponents/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../CSS/header.css';
+const Header = () => {
+  return (
+    <>a
+
+      <div className="header">
+        <div className="header_nav">
+          <div className="contentnav">
+            <div className="logo">
+              <h1>LOGO</h1>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default Header
